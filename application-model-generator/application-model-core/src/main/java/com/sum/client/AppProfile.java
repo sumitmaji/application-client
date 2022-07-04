fe86cdec74043ff2ade0fa9dac215abd063257d1
sumit <skmaji1@outlook.com>
2022-07-04 21:38:03 +0530

    Added hibernate annotation

---
+++ application-model-generator/application-model-core/src/main/java/com/sum/client/AppProfile.java
@@ -0,0 +1,39 @@
+package com.sum.client;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "app_profile")
+@NamedQueries({
+        @NamedQuery(name = "AppProfile.findByName", query = "select a from AppProfile a where a.name = :name")
+})
+@NoArgsConstructor
+public class AppProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Hibernate")
+    @SequenceGenerator(name = "Hibernate", sequenceName = "Hibernate_Sequence", allocationSize = 1)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    @PostLoad
+    public void postRemove() {
+
+    }
+}
